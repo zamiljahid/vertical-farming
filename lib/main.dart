980da78e70534b5be54a33643e3ca4cfedb9ba1b
mehedihasan <zamiljahid2002@gmail.com>
2025-06-03 17:14:15 +0600

    firebase added

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:vertical_farming/screens/splash_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   // await SharedPrefs.init();
   runApp(const ProviderScope(child: MainClass()));
 }
